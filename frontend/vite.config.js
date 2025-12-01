bc8d8675e8bee9f4d240b5f25657c608fb4b4b91
RuchaAhire <ahirerucha81@gmail.com>
2025-12-01 21:34:27 +0530

    added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import {defineConfig} from 'vite'
+// import {defineConfig} from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: process.env.VITE_BASE_PATH || "/Smart_AI_Beauty"
+// })
+
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Smart_AI_Beauty"
+  base: process.env.VITE_BASE_PATH || "/"
 })
